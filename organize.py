596a2c4b73097a7fe39201899ebe9395ce5c861c
Aman Patel <44617932+amanptl@users.noreply.github.com>
2020-05-12 11:33:26 -0400

    Update organize.py

--- organize.py
+++ organize.py
@@ -6,12 +6,12 @@ import json
 import time
 import argparse
 
-# Handler that will detect new files.
+# Look for new files
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         org()
 
-# Organizes by detecting the file types from the file name
+# Organize by extracting the file types from the file name
 def org():
     for file in os.listdir(folder_to_track):
         if os.path.isfile(os.path.join(folder_to_track, file)):
@@ -19,10 +19,10 @@ def org():
             for types in generalize:
                 if os.path.splitext(file)[1].lower() in generalize[types]:
                     dest = folder_to_track + folders[types]
-                    print(file, dest)
+                    print(file, dest) # Console log the file moved
                     move_file(src, dest, file)
 
-# Moves the file and creates the sub-directory if it doesn't exist
+# Move the file and create the sub-directory if it doesn't exist
 def move_file(src,dest,file):
     print("Moving file " + file + " to " + dest)
     if not os.path.exists(dest):
@@ -34,7 +34,7 @@ def move_file(src,dest,file):
     else:
         print("Moved " + file)
 
-# Creates the sub-direcotry
+# Create the sub-direcotry
 def create_dir(dir):
     try:
         os.mkdir(dir)
@@ -59,7 +59,7 @@ folders = {
     'word' : 'Word Document',
     'app' : 'Applications'
 }
-
+# Setting up the argument parser
 parser = argparse.ArgumentParser(usage="python organize.py <your_directory>")
 parser.add_argument('dir', type=str)
 args = parser.parse_args()
@@ -72,6 +72,7 @@ observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
 observer.start()
 
+# Run until user interrupts with Ctrl + C
 try:
     while True:
         time.sleep(0.1)
